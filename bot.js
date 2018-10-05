25eebe614de2bb934dccc1ffdaf64efd1cd0350f
SneaksterBloxDev <43881760+SneaksterBloxDev@users.noreply.github.com>
2018-10-05 14:01:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,8 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong')
+    	message.reply('pong');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
